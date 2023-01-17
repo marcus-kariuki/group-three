5d32150d91dfccb2b808186371440a733a1f81d6
Marcus <marcus.kariuki@student.moringaschool.com>
2023-01-17 23:53:59 +0300

    group three deployment

---
+++ src/components/Member.jsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+
+const Member = ({ id, dob, dod, type, name, image, founded, description }) => {
+    description = description.length > 100
+        ? `${(description || '').substring(0, 100)} ...`
+        : description;
+
+    return <Link
+        to={type === "individual" ? `/individuals/${id}` : `/groups/${id}`}
+        style={{ display: "flex", textDecoration: "none" }}>
+        <div className="card">
+            <div className="rounded-top" style={{
+                height: 240,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+                backgroundImage: `url(${image})`
+            }}>
+
+            </div>
+
+            <div className="card-body">
+                <h5 className="card-title text-dark">{name}</h5>
+
+                <div className="card-text">
+                    <p className="lead text-dark">
+                        {description}
+                    </p>
+
+                    {
+                        type === "individual"
+                            ? <div className="d-flex justify-content-between">
+                                <strong className="text-warning">Born: {dob}</strong>
+                                <strong className="text-danger">Died: {dod}</strong>
+                            </div>
+                            : <div className="d-flex justify-content-between">
+                                <strong className="text-primary">Founded: {founded}</strong>
+                            </div>
+                    }
+                </div>
+
+
+
+            </div>
+        </div>
+    </Link>;
+}
+
+export default Member;
